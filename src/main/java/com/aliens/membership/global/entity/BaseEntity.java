203b7cede563f037b81ecf35cec8e9ad10e922b8
김명준 <86913355+mjj111@users.noreply.github.com>
2023-12-11 16:15:16 +0900

    feat(BaseEntity): 엔티티에 공통적으로 들어갈 id, 생성날짜, 업데이트 날짜를 담은 인티티클래스 추가

---
+++ src/main/java/com/aliens/membership/global/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.aliens.membership.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
