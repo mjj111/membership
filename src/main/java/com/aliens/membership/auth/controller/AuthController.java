de3a531db897d65108606b054c35263207e721f5
김명준 <86913355+mjj111@users.noreply.github.com>
2023-12-11 16:17:51 +0900

    feat(AuthControllerTest): 로그인과 토큰 재발급에 대한컨트롤러 추가

---
+++ src/main/java/com/aliens/membership/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.aliens.membership.auth.controller;
+
+import com.aliens.membership.auth.dto.AuthTokenDto;
+import com.aliens.membership.auth.dto.LoginRequestDto;
+import com.aliens.membership.auth.dto.MemberInfoForToken;
+import com.aliens.membership.auth.service.AuthService;
+import com.aliens.membership.global.api.Api;
+import com.aliens.membership.global.custom.LoginMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping()
+    public Api<AuthTokenDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return Api.OK(authService.login(loginRequestDto));
+    }
+
+    @GetMapping()
+    public Api<MemberInfoForToken> getMemberInfo(@LoginMember MemberInfoForToken memberInfoForToken) {
+        return Api.OK(memberInfoForToken);
+    }
+}
