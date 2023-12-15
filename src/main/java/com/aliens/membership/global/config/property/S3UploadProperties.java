781b5b2cd92618cb44754f960ef190b7699be48a
김명준 <86913355+mjj111@users.noreply.github.com>
2023-12-15 18:45:46 +0900

    feat(S3UploadProperties) : AWS S3 접근을 위한 환경변수를 담은 클래스 생성

---
+++ src/main/java/com/aliens/membership/global/config/property/S3UploadProperties.java
@@ -0,0 +1,24 @@
+package com.aliens.membership.global.config.property;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Getter
+public class S3UploadProperties {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+}
